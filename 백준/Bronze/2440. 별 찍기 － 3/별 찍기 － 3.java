a8f872c89f5c6fa0ccd7cdebe7dd7f3becfec1b8
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-05-09 01:11:33 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 232 ms, Memory: 14944 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
