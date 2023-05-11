6916a4b4abc1fc0ed1674abc3a18b58909d4f8c6
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-05-11 20:46:34 +0900

    [Silver I] Title: 포도주 시식, Time: 156 ms, Memory: 15892 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static Integer[] dp;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        dp = new Integer[N + 1];
+        arr = new int[N + 1];   // 현재 와인잔의 값
+
+        for(int i = 1; i < N+1; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[0] = 0;
+        dp[1] = arr[1];
+
+        if (N > 1) {
+            dp[2] = arr[1] + arr[2];
+        }
+        System.out.println(recur(N));
+    }
+    static int recur(int N) {
+        if (dp[N] == null) {
+            dp[N] = Math.max(Math.max(recur(N - 2), recur(N - 3) + arr[N - 1]) + arr[N], recur(N - 1));
+        }
+        return dp[N];
+    }
+}
