138ebd8d49100e0d6018de9c6b40e7dfefd982f1
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-05-18 18:31:56 +0900

    [Silver II] Title: 연속합, Time: 308 ms, Memory: 40176 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -15,14 +15,14 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         arr = new int[n];
         dp = new Integer[n];
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp[0] = arr[0];
+        dp[0] = arr[0]; // dp[0]은 첫 원소로 이전에 탐색할 게 없기 때문에 arr[0]으로 초기화
         max = arr[0];
 
         recur(n-1); // dp의 마지막 index는 n-1이므로 n-1부터 탐색
